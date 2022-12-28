f7830eefc3d7625e85ac19d7942ce69235c05196
Rifardo <rifardomatematicas@gmail.com>
2022-12-27 23:56:34 -0600

    Agregando instalaciones de Nerd Fonts

--- 01-Instalaciones-Configuraciones-Iniciales/ej1.sh
+++ 01-Instalaciones-Configuraciones-Iniciales/ej1.sh
@@ -1,7 +1,7 @@
 #!/bash/sh
 #PENDIENTE AGREGAR LAS INSTALACIONES 
 sudo pacman -S qtile xterm code firefox rofi which   nitrogen ttf-dejavu ttf-liberation noto-fonts pulseaudio pamixer arandr udiskie ntfs-3g network-manager-applet volumeicon cbatticon   xorg-xinit base-devel git thunar ranger glib2 gvfs lxappearance picom vlc neofetch htop python-psutil --noconfirm &&
-yay -S nerd-fonts-complete &&
+yay -S nerd-fonts-complete nerd-fonts-complete-mono-glyphs nerd-fonts-complete-starship &&
 sudo rm -r /home/rifardo/.config/qtile/ && 
 cp -r qtile/ /home/rifardo/.config/ && 
 cp autostart.sh /home/rifardo/.config/qtile/ &&
